37ccd5bd0e3196c0db3837a8c305fcfbb7e254fc
sashasohrabi <shb.sohrabi@gmail.com>
2019-01-18 23:44:44 +0200

    form validation.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
-console.log("hell");
+$(".contact-form").validate({
+  rules: {
+    email: {
+      required: true,
+      email: true
+    }
+  }, //end rules
+  messages: {
+    email: {
+      required: "Please supply an e-mail address.",
+      email: "This is not a valid email address."
+    }
+  }
+}); // end validate
